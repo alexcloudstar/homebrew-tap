50efc1b281ed62afc5172b37db426564932a5c44
alexcloudstar <alexcloudstar@gmail.com>
2024-03-30 00:09:05 +0200

    init commit

---
+++ Formula/tle.rb
@@ -0,0 +1,14 @@
+class tstpmove < Formula
+  desc "CLI gather & move all types with a specific extension to a specific directory"
+  homepage "https://github.com/alexcloudstar/tstpmove"
+  url "https://github.com/alexcloudstar/tstpmove/releases/download/v0.1.0/tstpmove-0.1.0-x86_64-apple-darwin.tar.gz"
+  sha256 "9754f6a5aee27ce1609f9f235769819afca17300"
+  version "0.1.0"
+
+  def install
+    bin.install "tstpmove"
+  end
+end
+
+
+
