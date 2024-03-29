a5cdb31d880618bb6961bb47257a184d1ae2c38b
alexcloudstar <alexcloudstar@gmail.com>
2024-03-30 00:20:36 +0200

    Bumb V to 1.0.0

--- Formula/tstpmove.rb
+++ Formula/tstpmove.rb
@@ -1,9 +1,9 @@
 class Tstpmove < Formula
   desc "CLI gather & move all types with a specific extension to a specific directory"
   homepage "https://github.com/alexcloudstar/tstpmove"
-  url "https://github.com/alexcloudstar/tstpmove/releases/download/v0.1.0/tstpmove-0.1.0-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/alexcloudstar/tstpmove/releases/download/v1.0.0/tstpmove-1.0.0-x86_64-apple-darwin.tar.gz"
   sha256 "9754f6a5aee27ce1609f9f235769819afca17300"
-  version "0.1.0"
+  version "1.0.0"
 
   def install
     bin.install "tstpmove"
