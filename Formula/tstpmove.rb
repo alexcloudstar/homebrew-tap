14a454af4536c1c53ca929a4341fb6a917dd0c5a
alexcloudstar <alexcloudstar@gmail.com>
2024-03-30 00:15:30 +0200

    fix: class/module name must be CONSTANT

--- Formula/tstpmove.rb
+++ Formula/tstpmove.rb
@@ -1,4 +1,4 @@
-class tstpmove < Formula
+class Tstpmove < Formula
   desc "CLI gather & move all types with a specific extension to a specific directory"
   homepage "https://github.com/alexcloudstar/tstpmove"
   url "https://github.com/alexcloudstar/tstpmove/releases/download/v0.1.0/tstpmove-0.1.0-x86_64-apple-darwin.tar.gz"
