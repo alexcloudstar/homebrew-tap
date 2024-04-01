1ed200905b89691facb3f5194219866a3342f0dd
alexcloudstar <alexcloudstar@gmail.com>
2024-04-01 21:09:59 +0300

    fix: sha256

--- Formula/tstpmove.rb
+++ Formula/tstpmove.rb
@@ -2,7 +2,7 @@ class Tstpmove < Formula
   desc "CLI gather & move all types with a specific extension to a specific directory"
   homepage "https://github.com/alexcloudstar/tstpmove"
   url "https://github.com/alexcloudstar/tstpmove/releases/download/1.2.2/tstpmove-1.2.2-x86_64-apple-darwin.tar.gz"
-  sha256 "c21983214d88f27a4bf73eddbe152b728c94b444"
+  sha256 "eb8ff44086c489da54289ce97cfba914764fe51455ce5740559ff88e74de1073"
   version "1.2.2"
 
   def install
